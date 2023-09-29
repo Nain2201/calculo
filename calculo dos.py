ee485ac803d75c48fa38f418eb050bb1f50d28f5
Nain <ne.villacisd@uea.edu.ec>
2023-09-29 09:17:43 -0500

    calcular_descuento

---
+++ calculo dos.py
@@ -0,0 +1,9 @@
+# Ejemplo 1: Calcular el descuento con un 10% predeterminado
+monto_compra = 150.0
+descuento = calcular_descuento(monto_compra)
+print(f"Descuento: ${descuento:.2f}")
+
+# Ejemplo 2: Calcular el descuento con un 20%
+monto_compra = 200.0
+descuento = calcular_descuento(monto_compra, 20)
+print(f"Descuento: ${descuento:.2f}")
